8622dff4f490783a32e44bd012635bdef7d804f4
andrew.shine <andrew.shine@MU-C1-10.mullen.pvt.k12.co.us>
2019-02-19 15:00:26 -0700

    Began levelreader

--- Sword/src/SwordRunner.java
+++ Sword/src/SwordRunner.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.Timer;
@@ -9,8 +11,59 @@ import java.util.Scanner;
 
 public class SwordRunner extends JPanel
 	{
+		public ArrayList<ArrayList<String>> level = new ArrayList<ArrayList<String>>();
 		public static void main(String[] args)
 			{
-				
+				JFrame frame = new JFrame("Sword");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setSize(1013, 913);
+				SwordRunner game = new SwordRunner();
+				frame.add(game);
+				frame.setVisible(true);
+				frame.setResizable(false);
+				game.setFocusable(true);
 			}
+		public SwordRunner()
+		{
+			setBackground(Color.LIGHT_GRAY);
+			Timer timer = new Timer(10, new ActionListener(){
+				public void actionPerformed(ActionEvent e)
+				{
+					repaint();
+				}
+			});
+		}
+		
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+		}
+		
+		public void readLevel()
+		{
+			Scanner levelReader = null;
+			int position = 0;
+			try
+				{
+					levelReader = new Scanner(new File("level.txt"));
+				} catch (FileNotFoundException e)
+				{
+//					System.out.println("Level not found");
+					System.out.println("BET");
+				}
+			while(levelReader.hasNextLine())
+				{
+					String levelLine = levelReader.nextLine();
+					char[] levelArray = levelLine.toCharArray();
+					for(char c: levelArray)
+						{
+							switch(c)
+							{
+								case 'g':
+//									level
+							}
+						}
+				}
+			
+		}
 	}
