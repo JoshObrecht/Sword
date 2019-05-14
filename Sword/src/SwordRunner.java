8459d1d584511d377379f52e10e800510f856bd7
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2019-05-14 14:57:39 -0600

    level 4 done

--- Sword/src/SwordRunner.java
+++ Sword/src/SwordRunner.java
@@ -36,7 +36,7 @@ public class SwordRunner extends JPanel
 		public final int size = 40;
 		public Entity skybox1;
 		public Entity skybox2;
-		public int levelNum = 0;
+		public int levelNum = 3;
 		public int stage = 0;
 		public int tickNum = 0;
 		public int letterNum = 0;
